0a905eb7570ce735ce90fa000535e4d2a5244003
ArtemMikhal <141834394+ArtemMikhal@users.noreply.github.com>
2024-01-31 22:30:59 +0300

    Создал приложения проекта

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'customers',
+    'mailing',
+    'messages',
+    'mailing_logs',
+    'blog',
 ]
 
 MIDDLEWARE = [
